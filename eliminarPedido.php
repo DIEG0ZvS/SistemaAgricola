2b080b89f0285ddb712b41bbf1d52e39b4d76118
Max <maxsalvador81@gmail.com>
2025-07-05 10:16:03 -0500

    modelo

--- eliminarPedido.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "controladores/PedidoController.php";
-
-if (!empty($_GET["id"])) {
-    $id = $_GET["id"];
-    $pc = new PedidoController();
-    $pc->eliminar($id);
-    header("Location: verPedido.php");
-}
