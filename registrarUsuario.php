cef302e6689127899e3e1adc8b1117b6efcb58e8
luzbuendia <bluz5695@gmail.com>
2025-07-05 18:35:24 -0500

    Vista

--- registrarUsuario.php
+++ registrarUsuario.php
@@ -3,12 +3,6 @@
     <input type="text" name="nombre" placeholder="Nombre"/><br>
     <input type="email" name="correo" placeholder="Correo"/><br>
     <input type="password" name="clave" placeholder="Clave"/><br>
-    <label for="rol">Rol:</label>
-    <select name="rol" id="rol">
-        <option value="empleado">Empleado</option>
-        <option value="vendedor">Vendedor</option>
-        <option value="administrador">Administrador</option>
-    </select><br>
     <input type="submit" value="Guardar"/>
 </form>
 
@@ -18,7 +12,6 @@ if (!empty($_POST)) {
     $nombre = trim($_POST["nombre"]);
     $correo = trim($_POST["correo"]);
     $clave = trim($_POST["clave"]);
-    $rol = trim($_POST["rol"]);
 
     $contadorErrores = 0;
 
@@ -43,11 +36,10 @@ if (!empty($_POST)) {
         $contadorErrores++;
     }
 
-
     if ($contadorErrores == 0) {
         require_once "controladores/UsuarioController.php";
         $uc = new UsuarioController();
-        $respuesta = $uc->guardar($nombre, $correo, $clave, $rol);
+        $respuesta = $uc->guardar($nombre, $correo, $clave);
         if ($respuesta) {
             echo "Usuario creado exitosamente";
         } else {
@@ -55,5 +47,4 @@ if (!empty($_POST)) {
         }
     }
 }
-
 ?>
