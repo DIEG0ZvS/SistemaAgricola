fc26e66bb65d543577df9399385855634f5f0d72
luzbuendia <bluz5695@gmail.com>
2025-07-05 19:57:59 -0500

    Cambio2

--- eliminarProducto.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "controladores/ProductoController.php";
-
-if (!empty($_GET["id"])) {
-    $id = $_GET["id"];
-    $pc = new ProductoController();
-    $pc->eliminar($id);
-    header("Location: verProducto.php");
-}
